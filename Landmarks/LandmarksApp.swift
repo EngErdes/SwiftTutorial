e6eaa3f48a8ed4c8277ced9b0940e6ca1f8928ec
中司浩史 <erdes@nakatsukasahirofuminoMacBook-Air.local>
2026-05-06 22:54:58 +0900

    initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 中司浩史 on 2026/05/06.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
